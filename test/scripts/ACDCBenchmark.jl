cf50d71b2b1e32a0f86caebb956c6b6fcaa30d4d
hakanergun <hakan.ergun@ieee.org>
2018-04-17 16:26:21 +0200

    test with case5dc b=0

--- test/scripts/ACDCBenchmark.jl
+++ test/scripts/ACDCBenchmark.jl
@@ -7,13 +7,14 @@ using Mosek
 
 files =
 [
-"./test/data/case5_dc.m"; #Don't use normally
-  "./test/data/case5_2grids.m";
-  "./test/data/case5_acdc.m";
-  "./test/data/case5_b2bdc.m";
-  "./test/data/case5_dcgrid.m";
-  "./test/data/case24_3zones_acdc.m";
-  "./test/data/case39_acdc.m";
+# "./test/data/case5_2grids.m";
+# "./test/data/case5_acdc.m";
+# "./test/data/case5_b2bdc.m";
+# #"./test/data/case5_dc.m"; #Don't use normally
+# "./test/data/case5_dcgrid.m";
+"./test/data/case5_dcgrid_b0.m";
+# "./test/data/case24_3zones_acdc.m";
+# "./test/data/case39_acdc.m";
 #"./test/data/case3120sp_acdc.m";
 ]
 
@@ -40,6 +41,24 @@ function calc_gap(dict)
     end
 end
 
+
+#
+function fix_things!(data)
+    # fix number of poles
+    data["dcpol"] = 1
+    #tap = 1
+    for (i,conv) in data["convdc"]
+        # remove transformer and phase reactor from relaxation
+        # conv["reactor"] = 0
+        #conv["transformer"] = 0
+        #conv["filter"] = 0
+        #conv["tm"] = 1
+        #conv["LossA"] = 0
+        #conv["LossB"] = 0
+        #conv["LossC"] = 0
+    end
+end
+
 for file in files
     data = PowerModels.parse_file(file)
     PowerModelsACDC.process_additional_data!(data)
@@ -62,9 +81,9 @@ for file in files
     resultSOCBFM = run_acdcopf(data, SOCDFPowerModel, ipopt; setting = s)
     case["BFM SOC"] = exctract_info(resultSOCBFM)
     # #
-    resultSDP = run_acdcopf(data, SDPWRMPowerModel, mosek; setting = s)
-    case["BIM SDP"] = exctract_info(resultSDP)
-    #case["BIM SDP"] = exctract_info(resultSOCBFM)
+    #resultSDP = run_acdcopf(data, SDPWRMPowerModel, mosek; setting = s)
+    #case["BIM SDP"] = exctract_info(resultSDP)
+    case["BIM SDP"] = exctract_info(resultSOCBFM)
     # # #
     resultDC = run_acdcopf(data, DCPPowerModel, mosek; setting = s)
     case["DC LP"] = exctract_info(resultDC)
@@ -81,22 +100,22 @@ function print_table_opt_gap(dict)
     s = s*"case"*c*"AC NLP"*c* "QC SOC" *c*c* "QCTri SOC" *c*c* "BIM SOC" *c*c* "BFM SOC" *c*c* "BIM SDP" *c*c* "DC LP"*c*raw"\ "[1]*raw"\ "* " \n"
     for (filename, ff) in dict
         s = s*filename
-        l = 3
-        l2 = 3
+        l = 5
+        l2 = 7
         p = 100
-        s = s *c*string(round(ff["AC NLP"]["obj"], l))
-        s = s *c*string(round(ff["BIM SDP"]["obj"], l))
-        s = s *c*string(round(p*ff["BIM SDP"]["gap"], l2))
-        s = s *c*string(round(ff["QC SOC"]["obj"], l))
-        s = s *c*string(round(p*ff["QC SOC"]["gap"], l2))
+        s = s *c*string(ff["AC NLP"]["obj"])[1:l2]
+        s = s *c*string(ff["BIM SDP"]["obj"])[1:l2]
+        s = s *c*string(p*ff["BIM SDP"]["gap"])[1:l]
+        s = s *c*string(ff["QC SOC"]["obj"])[1:l2]
+        s = s *c*string(p*ff["QC SOC"]["gap"])[1:l]
         # s = s *c*string(ff["QCTri SOC"]["obj"])[1:l2]
         # s = s *c*string(p*ff["QCTri SOC"]["gap"])[1:l]
-        s = s *c*string(round(ff["BIM SOC"]["obj"], l))
-        s = s *c*string(round(p*ff["BIM SOC"]["gap"], l2))
-        s = s *c*string(round(ff["BFM SOC"]["obj"], l))
-        s = s *c*string(round(p*ff["BFM SOC"]["gap"], l2))
-        s = s *c*string(round(ff["DC LP"]["obj"], l))
-        s = s *c*string(round(p*ff["DC LP"]["gap"], l2))
+        s = s *c*string(ff["BIM SOC"]["obj"])[1:l2]
+        s = s *c*string(p*ff["BIM SOC"]["gap"])[1:l]
+        s = s *c*string(ff["BFM SOC"]["obj"])[1:l2]
+        s = s *c*string(p*ff["BFM SOC"]["gap"])[1:l]
+        s = s *c*string(ff["DC LP"]["obj"])[1:l2]
+        s = s *c*string(p*ff["DC LP"]["gap"])[1:l]
 
         s = s*raw"\ "[1]*raw"\ "*" \n"
     end
