8df9ea98d76587b52a7da9bc5a9ac09eb03722e5
Hakan Ergun <hakan.ergun@ieee.org>
2020-07-28 16:56:03 +0200

    Fix document make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -17,7 +17,7 @@ makedocs(
             "Modeling Components" => [
                 "Objective" => "objective.md",
                 "Variables" => "variables.md",
-                "Constraints" => "constraints.md"
+                "Constraints" => "constraints.md",
                 "Problem Types" => "problems.md"
             ],
             "File IO" => "parser.md"
@@ -26,9 +26,5 @@ makedocs(
 )
 
 deploydocs(
-    # deps = nothing,
-    # make = nothing,
-    # target = "build",
-    # julia = "1.1",
     repo = "github.com/hakanergun/PowerModelsACDC.jl.git"
 )
