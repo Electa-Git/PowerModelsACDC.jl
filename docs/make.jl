64be99bd39ba59eccf6c032d1c66430e04bf58ad
Hakan Ergun <hakan.ergun@ieee.org>
2020-07-29 09:17:23 +0200

    Create make.jl

---
+++ docs/make.jl
@@ -0,0 +1,30 @@
+using Documenter, PowerModelsACDC
+
+makedocs(
+    modules = PowerModelsACDC,
+    format = Documenter.HTML(),
+    sitename = "PowerModelsACDC",
+    authors = "Frederik Geth, Hakan Ergun",
+    pages = [
+        "Home" => "index.md",
+        "Manual" => [
+            "Getting Started" => "quickguide.md",
+            "Results" => "result-data.md",
+        ],
+        "Library" => [
+            "Network Formulations" => "formulations.md",
+            "Problem Specifications" => "specifications.md",
+            "Problem Types" => "problems.md",
+            "Modeling Components" => [
+                "Objective" => "objective.md",
+                "Variables" => "variables.md",
+                "Constraints" => "constraints.md",
+            ],
+            "File IO" => "parser.md"
+        ],
+    ]
+)
+
+deploydocs(
+    repo = "github.com/hakanergun/PowerModelsACDC.jl.git",
+)
