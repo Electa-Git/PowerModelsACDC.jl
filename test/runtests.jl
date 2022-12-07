09ffa667ba76e6ddd256d9dabf92ea5ff62262e8
Hakan Ergun <hakan.ergun@ieee.org>
2022-12-07 17:42:32 +0100

    fix tests and highs dependecy

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using PowerModels
 using Memento
 using InfrastructureModels
 using JuMP
-using HiGHS
+
 
 # Suppress warnings during testing.
 Memento.setlevel!(Memento.getlogger(InfrastructureModels), "error")
@@ -12,13 +12,12 @@ Memento.setlevel!(Memento.getlogger(PowerModels), "error")
 
 using Ipopt
 using Juniper
-
+using HiGHS
 using Test
 
 local_test = false   # as some tests require Mosek, only limited set sent to CI.
 
 ipopt_solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "tol" => 1e-6, "print_level" => 0)
-scs_solver = JuMP.optimizer_with_attributes(SCS.Optimizer, "verbose" => 0)
 highs = JuMP.optimizer_with_attributes(HiGHS.Optimizer)
 juniper = JuMP.optimizer_with_attributes(Juniper.Optimizer, "nl_solver" => ipopt_solver, "mip_solver" => highs, "time_limit" => 7200)
 
