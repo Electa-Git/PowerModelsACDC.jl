303187c0fab3e5c5fd763f7b59e49aa4af4a6eb3
Matteo Rossini <matteo.rossini@kuleuven.be>
2026-05-30 11:47:44 +0200

    Unit tests: fix imports and clean up
    - Fix imports: use `using` for PowerModelsACDC and `import` for all other packages.
    - Remove unnecessary import statements.
    - Locate files independently of the current working directory using `pkgdir`.
    - Clean up the code of the main files.

--- test/common.jl
+++ test/common.jl
@@ -1,11 +1,11 @@
 function build_mn_data(file_name)
     mp_data = PowerModels.parse_file(file_name)
-    mp_data1 = PowerModels.replicate(mp_data, 2; global_keys = Set{String}(["source_type", "name", "source_version", "per_unit"]))
-    process_additional_data!(mp_data1; tnep = true)
+    mp_data1 = PowerModels.replicate(mp_data, 2; global_keys=Set{String}(["source_type", "name", "source_version", "per_unit"]))
+    process_additional_data!(mp_data1; tnep=true)
     return mp_data1
 end
 
-function prepare_uc_test_data(file; contingencies = nothing, tielines = nothing)
+function prepare_uc_test_data(file; contingencies=nothing, tielines=nothing)
     # Parse file using PowerModels
     data = PowerModels.parse_file(file)
     # Process demand reduction and curtailment data
@@ -21,21 +21,21 @@ function prepare_uc_test_data(file; contingencies = nothing, tielines = nothing)
     end
 
     data, frequency_parameters = add_fcuc_data!(data)
-    prepare_uc_data!(data; uc = true, frequency_parameters = frequency_parameters)
+    prepare_uc_data!(data; uc=true, frequency_parameters=frequency_parameters)
 
     number_of_hours = 24
 
-    g_series = [0.4  0.5  0.66  0.7   0.7   0.9   0.95 1.02  1.15  1.3   1.35 1.3   1.21  1.08  1.0  0.96  0.93 1.0  1.1   1.2  1.08  1.05  0.99 0.89]
-    l_series = [0.6  0.7  0.75  0.78  0.85  0.88  0.9  1.0   1.12  1.25  1.2  1.08  0.99  0.92  0.8  0.73  0.8  0.9  1.03  1.2  1.11  0.99  0.8  0.69]
+    g_series = [0.4 0.5 0.66 0.7 0.7 0.9 0.95 1.02 1.15 1.3 1.35 1.3 1.21 1.08 1.0 0.96 0.93 1.0 1.1 1.2 1.08 1.05 0.99 0.89]
+    l_series = [0.6 0.7 0.75 0.78 0.85 0.88 0.9 1.0 1.12 1.25 1.2 1.08 0.99 0.92 0.8 0.73 0.8 0.9 1.03 1.2 1.11 0.99 0.8 0.69]
 
-    mn_data = create_multinetwork_uc_model!(data, number_of_hours, g_series, l_series, contingencies = contingencies)
+    mn_data = create_multinetwork_uc_model!(data, number_of_hours, g_series, l_series, contingencies=contingencies)
 
     return mn_data
 end
 
 
 function add_fcuc_data!(data)
-    frequency_parameters = Dict{String, Any}()
+    frequency_parameters = Dict{String,Any}()
     frequency_parameters["fmin"] = 49.0
     frequency_parameters["fmax"] = 51.0
     frequency_parameters["f0"] = 50.0
@@ -63,7 +63,7 @@ function add_fcuc_data!(data)
             storage["inertia_constants"] = 4.0
             storage["start_up_cost"] = 1000.0
             storage["ramp_rate"] = 1.0
-            storage["ramp_rate_per_s"] =   storage["ramp_rate"] / 3600
+            storage["ramp_rate_per_s"] = storage["ramp_rate"] / 3600
             storage["fcr_contribution"] = true
             storage["mdt"] = 1.0
             storage["mut"] = 1.0
@@ -86,29 +86,31 @@ function prepare_storage_opf_data(file)
         data["load"][l]["flex"] = 1
     end
 
-    # indeicate which generators are renewable
+    # indicate which generators are renewable
     for (g, gen) in data["gen"]
-    if g == "2" || g == "8"
+        if g == "2" || g == "8"
             data["gen"][g]["res"] = true
-    else
+        else
             data["gen"][g]["res"] = false
-    end
+        end
     end
 
     # create a matrix with time points where storage should have a fixed content (column 1), and the fixed energy content in p.u. (column 2)
     for (s, strg) in data["storage"]
-        data["storage"][s]["fixed_energy"] = [25 0.5
-                                            49 0.4
-                                            73 0.3
-                                            97 0.6]
+        data["storage"][s]["fixed_energy"] = [
+            25 0.5
+            49 0.4
+            73 0.3
+            97 0.6
+        ]
     end
 
     # We are going to consider 3 typical days which will make up in total five days. The sequence is [day1 day2 day1 day3 day1]
     number_of_hours = 120
 
-    # this is an arbutary generation and demand series, later replace with something more representative
-    g_series = [0.4  0.5  0.66  0.7   0.7   0.9   0.95 1.02  1.15  1.3   1.35 1.3   1.21  1.08  1.0  0.96  0.93 1.0  1.1   1.2  1.08  1.05  0.99 0.89]
-    l_series = [0.6  0.7  0.75  0.78  0.85  0.88  0.9  1.0   1.12  1.25  1.2  1.08  0.99  0.92  0.8  0.73  0.8  0.9  1.03  1.2  1.11  0.99  0.8  0.69]
+    # This is an arbitrary generation and demand series, later replace with something more representative
+    g_series = [0.4 0.5 0.66 0.7 0.7 0.9 0.95 1.02 1.15 1.3 1.35 1.3 1.21 1.08 1.0 0.96 0.93 1.0 1.1 1.2 1.08 1.05 0.99 0.89]
+    l_series = [0.6 0.7 0.75 0.78 0.85 0.88 0.9 1.0 1.12 1.25 1.2 1.08 0.99 0.92 0.8 0.73 0.8 0.9 1.03 1.2 1.11 0.99 0.8 0.69]
 
     # just define demand and generation time series for each representative day
     day1g = g_series
@@ -127,4 +129,3 @@ function prepare_storage_opf_data(file)
 
     return mn_data
 end
-
