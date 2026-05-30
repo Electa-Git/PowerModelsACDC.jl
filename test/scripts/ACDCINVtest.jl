dabcd407e7f8dd4380b92d5692d07949abcf4271
Matteo Rossini <matteo.rossini@kuleuven.be>
2026-05-30 16:59:44 +0200

    Move `display_results_tnep` to script
    The function `display_results_tnep` is only used in a script and has no tests or documentation. Therefore, it is best placed in the same script where it is used.

--- test/scripts/ACDCINVtest.jl
+++ test/scripts/ACDCINVtest.jl
@@ -38,7 +38,26 @@ resultSOCWR = solve_tnep(file, PowerModels.SOCWRPowerModel, gurobi, setting = s)
 resultQC     =  solve_tnep(file, PowerModels.QCRMPowerModel, gurobi; setting = s)
 # resultSDP     =  solve_tnep(file, PowerModels.SDPWRMPowerModel, mosek; setting = s)
 resultLPAC     =  solve_tnep(file, PowerModels.LPACCPowerModel, gurobi; setting = s)
-#
+
+# Example code to print list of built HVDC branches and converters
+function display_results_tnep(result)
+    built_cv = []
+    built_br = []
+    for (c, conv) in result["solution"]["convdc_ne"]
+        if isapprox(conv["isbuilt"] , 1; atol = 0.01)
+            print("Conv: $c \n")
+            push!(built_cv,c)
+        end
+    end
+    for (b, branch) in result["solution"]["branchdc_ne"]
+        if isapprox(branch["isbuilt"] , 1; atol = 0.01)
+            print("Branch: $b \n")
+            push!(built_br,b)
+        end
+    end
+    return built_cv, built_br
+end
+
 display_results_tnep(resultDC)
 display_results_tnep(resultAC)
 display_results_tnep(resultSOCBF)
