fa1851e01dc8149fc2dbbe886cbc1f70000fe6f9
Hakan Ergun <hakan.ergun@ieee.org>
2026-07-15 21:44:04 +0200

    clean up the UC model.

--- src/prob/rocofuc.jl
+++ src/prob/rocofuc.jl
@@ -23,47 +23,6 @@ function solve_rocofuc(data::Dict{String,Any}, model_type::Type, optimizer; kwar
 end
 
 """
-    build_rocofuc(pm::_PM.AbstractPowerModel)
-
-Construct the multi-network FCUC JuMP model.
-
-# Inputs
-- `pm::_PM.AbstractPowerModel` : PowerModels internal model holder with multi-network and time-series references.
-
-# Details
-- Adds network-scoped variables for AC voltages, DC grid variables, converters and
-  inertia/reserve contributions.
-- Calls `uc_model!` for each scheduling hour (from `pm.ref[:it][:pm][:hour_ids]`) to build
-  the per-hour UC submodel (branch power, storage, unit commitment variables, contingencies).
-- Calls `contingency_constraints!` for contingency stages (from `pm.ref[:it][:pm][:cont_ids]`).
-- Assembles the global objective via `objective_min_cost_fcuc(pm; droop = true)`.
-"""
-function build_rocofuc(pm::_PM.AbstractPowerModel)
-
-    for n in _PM.nw_ids(pm)
-        _PM.variable_bus_voltage(pm; nw = n)
-
-        variable_active_dcbranch_flow(pm; nw = n)
-        variable_dcbranch_current(pm; nw = n)
-        variable_dc_converter(pm; nw = n)
-        variable_dcgrid_voltage_magnitude(pm; nw = n)
-        variable_dcgenerator_power(pm; nw = n)
-
-        _PM.constraint_model_voltage(pm; nw = n)
-        constraint_voltage_dc(pm; nw = n)
-
-        variable_inertia(pm; nw = n)
-        _PM.variable_gen_power(pm; nw = n)
-    end
-
-    for n in pm.ref[:it][:pm][:hour_ids]
-        rocofuc_model!(pm, n)
-    end
-
-        objective_min_cost_uc(pm)
-end
-
-"""
     uc_model!(pm, n)
 
 Build the per-hour Unit Commitment (UC) submodel for network/time-index `n`.
@@ -83,79 +42,100 @@ Build the per-hour Unit Commitment (UC) submodel for network/time-index `n`.
 - Applies fixed cross-border flow constraints if enabled in `pm.setting`.
 - Adds RoCoF contingency constraints for defined system splits. (ToDo: later add different contingency types)
 """
-function rocofuc_model!(pm, n) 
-    _PM.variable_branch_power(pm; nw = n)
-    _PM.variable_storage_power(pm; nw = n)
-    variable_generator_states(pm; nw = n, uc = true)
-    variable_flexible_demand(pm; nw = n)
-    variable_pst(pm; nw = n)
-    variable_sssc(pm; nw = n)
-    variable_storage_on_off(pm; nw = n)
-    variable_split_contingency(pm; nw = n)
+ function build_rocofuc(pm) 
+    for n in _PM.nw_ids(pm)
+        _PM.variable_bus_voltage(pm; nw = n)
+        _PM.variable_branch_power(pm; nw = n)
+        _PM.variable_gen_power(pm; nw = n)
+        _PM.variable_storage_power(pm; nw = n)
+        _PM.constraint_model_voltage(pm; nw = n)
 
-    for i in _PM.ids(pm, n, :ref_buses)
-        _PM.constraint_theta_ref(pm, i; nw = n)
-    end
-
-    for i in _PM.ids(pm, n, :bus)
-        constraint_power_balance_ac(pm, i; nw = n) # add storage
-    end
-
-    for i in _PM.ids(pm, n, :branch)
-        _PM.constraint_ohms_yt_from(pm, i; nw = n)
-        _PM.constraint_ohms_yt_to(pm, i; nw = n)
-        _PM.constraint_voltage_angle_difference(pm, i; nw = n)
-        _PM.constraint_thermal_limit_from(pm, i; nw = n)
-        _PM.constraint_thermal_limit_to(pm, i; nw = n)
-    end
-    for i in _PM.ids(pm, n, :busdc)
-        constraint_power_balance_dc(pm, i; nw = n)
-    end
-    for i in _PM.ids(pm, n, :branchdc)
-        constraint_ohms_dc_branch(pm, i; nw = n)
-    end
-    for i in _PM.ids(pm, n, :convdc)
-        constraint_converter_losses(pm, i; nw = n)
-        constraint_converter_current(pm, i; nw = n)
-        constraint_conv_transformer(pm, i; nw = n)
-        constraint_conv_reactor(pm, i; nw = n)
-        constraint_conv_filter(pm, i; nw = n)
+        variable_active_dcbranch_flow(pm; nw = n)
+        variable_dcbranch_current(pm; nw = n)
+        variable_dc_converter(pm; nw = n)
+        variable_dcgrid_voltage_magnitude(pm; nw = n)
+        variable_dcgenerator_power(pm; nw = n)
+        variable_generator_states(pm; nw = n, uc = true)
+        variable_flexible_demand(pm; nw = n)
+        variable_pst(pm; nw = n)
+        variable_sssc(pm; nw = n)
+        variable_storage_on_off(pm; nw = n)
+        constraint_voltage_dc(pm; nw = n)
     end
 
-    for i in _PM.ids(pm, n, :flex_load)
-        constraint_total_flexible_demand(pm, i; nw = n)
-    end
-
-    if haskey(pm.setting, "fix_cross_border_flows") && pm.setting["fix_cross_border_flows"] == true
-        if !haskey(pm.setting, "borders")
-            borders = [i for i in _PM.ids(pm, n, :borders)]
-        else
-            borders = [i for i in pm.setting["borders"]]
+    for n in _PM.nw_ids(pm)
+        variable_split_contingency(pm; nw = n)
+        for i in _PM.ids(pm, n, :ref_buses)
+            _PM.constraint_theta_ref(pm, i; nw = n)
         end
-        for i in borders
-            constraint_fixed_xb_flows(pm, i; nw = n)
+
+        for i in _PM.ids(pm, n, :bus)
+            constraint_power_balance_ac(pm, i; nw = n)
         end
-    end
 
-    gen_status = haskey(pm.setting, "use_gen_status") && pm.setting["use_gen_status"] == true
-    for i in _PM.ids(pm, n, :gen)
-        constraint_generator_on_off(pm, i; nw = n, use_status = gen_status)
-        constraint_unit_commitment(pm, i; nw = n)
-    end
+        for i in _PM.ids(pm, n, :branch)
+            _PM.constraint_ohms_yt_from(pm, i; nw = n)
+            _PM.constraint_ohms_yt_to(pm, i; nw = n)
+            _PM.constraint_voltage_angle_difference(pm, i; nw = n)
+            _PM.constraint_thermal_limit_from(pm, i; nw = n)
+            _PM.constraint_thermal_limit_to(pm, i; nw = n)
+        end
+        for i in _PM.ids(pm, n, :busdc)
+            constraint_power_balance_dc(pm, i; nw = n)
+        end
+        for i in _PM.ids(pm, n, :branchdc)
+            constraint_ohms_dc_branch(pm, i; nw = n)
+        end
+        for i in _PM.ids(pm, n, :convdc)
+            constraint_converter_losses(pm, i; nw = n)
+            constraint_converter_current(pm, i; nw = n)
+            constraint_conv_transformer(pm, i; nw = n)
+            constraint_conv_reactor(pm, i; nw = n)
+            constraint_conv_filter(pm, i; nw = n)
+        end
 
-    for i in _PM.ids(pm, n, :pst)
-        constraint_ohms_y_from_pst(pm, i; nw = n)
-        constraint_ohms_y_to_pst(pm, i; nw = n)
-        constraint_limits_pst(pm, i; nw = n)
-    end
+        for i in _PM.ids(pm, n, :flex_load)
+            constraint_total_flexible_demand(pm, i; nw = n)
+        end
 
-    for i in _PM.ids(pm, n, :sssc)
-        constraint_ohms_y_from_sssc(pm, i; nw = n)
-        constraint_ohms_y_to_sssc(pm, i; nw = n)
-        constraint_limits_sssc(pm, i; nw = n)
-    end
+        for i in _PM.ids(pm, n, :gen)
+            constraint_generator_on_off(pm, i; nw = n, use_status = false)
+            constraint_unit_commitment(pm, i; nw = n)
+            if haskey(pm.setting, "uc_reserves") && pm.setting["uc_reserves"] == true
+                constraint_unit_commitment_reserves(pm, i; nw = n)
+            end
+        end
 
-    for i in _PM.ids(pm, n, :zones)
+        for i in _PM.ids(pm, n, :pst)
+            constraint_ohms_y_from_pst(pm, i; nw = n)
+            constraint_ohms_y_to_pst(pm, i; nw = n)
+            constraint_limits_pst(pm, i; nw = n)
+        end
+
+        for i in _PM.ids(pm, n, :sssc)
+            constraint_ohms_y_from_sssc(pm, i; nw = n)
+            constraint_ohms_y_to_sssc(pm, i; nw = n)
+            constraint_limits_sssc(pm, i; nw = n)
+        end
+
+        if haskey(pm.setting, "fix_cross_border_flows") && pm.setting["fix_cross_border_flows"] == true
+            if !haskey(pm.setting, "borders")
+                borders = [i for i in _PM.ids(pm, n,:borders)]
+            else
+                borders = [i for i in pm.setting["borders"]]
+            end
+            for i in borders
+                constraint_fixed_xb_flows(pm, i; nw = n)
+            end
+        end
+
+        if haskey(_PM.ref(pm, n), :storage)
+            storage_constraints(pm, n; uc = true)
+        end
+
+        for i in _PM.ids(pm, n, :zones)
         constraint_rocof_split(pm, i; nw = n)
+        end
     end
+    objective_min_cost_uc(pm)
 end
