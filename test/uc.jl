4e336089e0076babd7f33b1dcc324fd79155f82f
Matteo Rossini <matteo.rossini@kuleuven.be>
2026-07-22 17:19:06 +0200

    Improve unit test robustness
    - Use SCIP instead of Juniper where helpful to improve robustness and solution speed.
    - Improve tests for the system-split-constrained unit commitment problem:
        - Set `conv_losses_mp` to `false` to reduce model symmetry.
        - Choose a different network with higher DC branch power.
        - Swap the settings of the two test cases to match their respective descriptions.
    - Remove flaky tests on `case9_test`: because generator costs are zero, power flow results are unreliable in some formulations.
    - Require PowerModels v0.21.6 or later and InfrastructureModels v0.7.9 or later. Our implementation of the `silence` function requires the `logger_config!` method introduced in those dependency versions.

--- test/uc.jl
+++ test/uc.jl
@@ -3,13 +3,13 @@
     uc_data = prepare_uc_test_data(file; number_of_hours=12)
     @testset "No HVDC, no binary relaxation" begin
         s = Dict("conv_losses_mp" => true, "hvdc_inertia_contribution" => false, "relax_uc_binaries" => false)
-        result = solve_uc(uc_data, PowerModels.DCPPowerModel, highs, setting=s, multinetwork=true)
+        result = solve_uc(uc_data, PowerModels.DCPPowerModel, scip, setting=s, multinetwork=true)
         @test result["termination_status"] == OPTIMAL
         @test result["objective"] ≈ 5.3283e5 rtol=1e-3
     end
     @testset "No HVDC, with binary relaxation" begin
         s = Dict("conv_losses_mp" => true, "hvdc_inertia_contribution" => false, "relax_uc_binaries" => true)
-        result = solve_uc(uc_data, PowerModels.DCPPowerModel, highs, setting=s, multinetwork=true)
+        result = solve_uc(uc_data, PowerModels.DCPPowerModel, scip, setting=s, multinetwork=true)
         @test result["termination_status"] == OPTIMAL
         @test result["objective"] ≈ 5.3283e5 rtol=1e-3
     end
@@ -17,13 +17,13 @@
     uc_data = prepare_uc_test_data(file; number_of_hours=12)
     @testset "With HVDC, no binary relaxation" begin
         s = Dict("conv_losses_mp" => true, "hvdc_inertia_contribution" => true, "relax_uc_binaries" => false)
-        result = solve_uc(uc_data, PowerModels.DCPPowerModel, highs, setting=s, multinetwork=true)
+        result = solve_uc(uc_data, PowerModels.DCPPowerModel, scip, setting=s, multinetwork=true)
         @test result["termination_status"] == OPTIMAL
         @test result["objective"] ≈ 4.9748e5 rtol=1e-3
     end
     @testset "With HVDC, with binary relaxation" begin
         s = Dict("conv_losses_mp" => true, "hvdc_inertia_contribution" => true, "relax_uc_binaries" => true)
-        result = solve_uc(uc_data, PowerModels.DCPPowerModel, highs, setting=s, multinetwork=true)
+        result = solve_uc(uc_data, PowerModels.DCPPowerModel, scip, setting=s, multinetwork=true)
         @test result["termination_status"] == OPTIMAL
         @test result["objective"] ≈ 4.9748e5 rtol=1e-3
     end
@@ -31,13 +31,13 @@
     uc_data = prepare_uc_test_data(file; number_of_hours=12)
     @testset "With HVDC & storage, no binary relaxation" begin
         s = Dict("conv_losses_mp" => true, "hvdc_inertia_contribution" => true, "relax_uc_binaries" => false)
-        result = solve_uc(uc_data, PowerModels.DCPPowerModel, highs, setting=s, multinetwork=true)
+        result = solve_uc(uc_data, PowerModels.DCPPowerModel, scip, setting=s, multinetwork=true)
         @test result["termination_status"] == OPTIMAL
         @test result["objective"] ≈ 4.9679e5 rtol=1e-3
     end
     @testset "With HVDC & storage, with binary relaxation" begin
         s = Dict("conv_losses_mp" => true, "hvdc_inertia_contribution" => true, "relax_uc_binaries" => true)
-        result = solve_uc(uc_data, PowerModels.DCPPowerModel, highs, setting=s, multinetwork=true)
+        result = solve_uc(uc_data, PowerModels.DCPPowerModel, scip, setting=s, multinetwork=true)
         @test result["termination_status"] == OPTIMAL
         @test result["objective"] ≈ 4.9679e5 rtol=1e-3
     end
@@ -48,13 +48,13 @@ end
     fcuc_data = prepare_uc_test_data(file; contingencies=contingencies, number_of_hours=6)
     @testset "No DC actions" begin
         s = Dict("conv_losses_mp" => true, "relax_uc_binaries" => false, "uc_reserves" => false, "hvdc_inertia_contribution" => false, "fix_cross_border_flows" => false)
-        result = solve_fcuc(fcuc_data, PowerModels.DCPPowerModel, highs, setting=s, multinetwork=true)
+        result = solve_fcuc(fcuc_data, PowerModels.DCPPowerModel, scip, setting=s, multinetwork=true)
         @test result["termination_status"] == OPTIMAL
         @test result["objective"] ≈ 5.7793e6 rtol=1e-3
     end
     @testset "With DC actions" begin
         s = Dict("conv_losses_mp" => true, "relax_uc_binaries" => false, "uc_reserves" => false, "hvdc_inertia_contribution" => true, "fix_cross_border_flows" => false)
-        result = solve_fcuc(fcuc_data, PowerModels.DCPPowerModel, highs, setting=s, multinetwork=true)
+        result = solve_fcuc(fcuc_data, PowerModels.DCPPowerModel, scip, setting=s, multinetwork=true)
         @test result["termination_status"] == OPTIMAL
         @test result["objective"] ≈ 1.8343e5 rtol=1e-3
     end
@@ -64,17 +64,17 @@ end
     contingencies = Dict("type" => "N-1", "elements" => ["gen"])
     spcuc_data = prepare_uc_test_data(file; contingencies=contingencies, tielines=[8], number_of_hours=6)
     @testset "No split constraints" begin
-        s = Dict("conv_losses_mp" => true, "relax_uc_binaries" => false, "uc_reserves" => false, "hvdc_inertia_contribution" => true, "fix_cross_border_flows" => false, "add_split_constraints" => true)
-        result = solve_spcuc(spcuc_data, PowerModels.DCPPowerModel, juniper, setting=s, multinetwork=true)
-        @test result["termination_status"] == LOCALLY_SOLVED
+        s = Dict("conv_losses_mp" => false, "relax_uc_binaries" => false, "uc_reserves" => false, "hvdc_inertia_contribution" => false, "fix_cross_border_flows" => false, "add_split_constraints" => false)
+        result = solve_spcuc(spcuc_data, PowerModels.DCPPowerModel, scip, setting=s, multinetwork=true)
+        @test result["termination_status"] == OPTIMAL
         @test result["objective"] ≈ 1.1039e5 rtol=1e-3
-        @test result["solution"]["nw"]["16"]["branchdc"]["1"]["pf"] ≈ 0.1681 rtol=1e-3
+        @test result["solution"]["nw"]["36"]["branchdc"]["1"]["pf"] ≈ 1.0297 rtol=1e-3
     end
     @testset "With split constraints" begin
-        s = Dict("conv_losses_mp" => true, "relax_uc_binaries" => false, "uc_reserves" => false, "hvdc_inertia_contribution" => false, "fix_cross_border_flows" => false, "add_split_constraints" => false)
-        result = solve_spcuc(spcuc_data, PowerModels.DCPPowerModel, juniper_warmstart, setting=s, multinetwork=true)
-        @test result["termination_status"] == LOCALLY_SOLVED
+        s = Dict("conv_losses_mp" => false, "relax_uc_binaries" => false, "uc_reserves" => false, "hvdc_inertia_contribution" => true, "fix_cross_border_flows" => false, "add_split_constraints" => true)
+        result = solve_spcuc(spcuc_data, PowerModels.DCPPowerModel, scip, setting=s, multinetwork=true)
+        @test result["termination_status"] == OPTIMAL
         @test result["objective"] ≈ 1.1039e5 rtol=1e-3
-        @test result["solution"]["nw"]["16"]["branchdc"]["1"]["pf"] ≈ 0.1133 rtol=1e-3
+        @test result["solution"]["nw"]["36"]["branchdc"]["1"]["pf"] ≈ 1.0297 rtol=1e-3
     end
 end
